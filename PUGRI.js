64726190ad829589a4fea3ecd059cfc324ef670f
Daniel Allott <36487931+Allott@users.noreply.github.com>
2020-04-07 17:42:40 +0100

    Create PUGRI.js

---
+++ PUGRI.js
@@ -0,0 +1,17 @@
+let raceBox = document.getElementById("race-box")
+
+function roll() {
+  randomRace()
+}
+
+function randomRace() {
+  raceBox.innerHTML = Math.floor(Math.random() * 20)
+}
+
+function randomClass1() {
+  
+}
+
+function randomClass2() {
+  
+}
